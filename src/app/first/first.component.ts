0b0c679f3c02eaef28c758e2c2c0d3142f9db67c
Hung Tran (Web) <hung.tran@bbv.vn>
2023-01-31 14:55:11 +0700

    feat: demo query params

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-first',
   templateUrl: './first.component.html',
-  styleUrls: ['./first.component.css']
+  styleUrls: ['./first.component.css'],
 })
-export class FirstComponent {
+export class FirstComponent implements OnInit {
+  constructor(private readonly route: ActivatedRoute) {}
 
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      console.log(params['name']);
+    });
+  }
 }
